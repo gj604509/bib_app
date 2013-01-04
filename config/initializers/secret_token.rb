21c6b046ba0999c85798737d3247e5a40f03d4e8
Jean-Charles Gastaud <unice@jc.cx>
2013-01-04 10:04:15 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BibApp::Application.config.secret_token = '92c625282646765d7dfe45d0c4e5660bfa8842654b1814e66d315665a819323cfb3c0dd6aad29de6b1993d6f334415687011243ab3d7d9ca2b4b59ab5e071683'
