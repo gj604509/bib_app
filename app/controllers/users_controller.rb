8ba8f464616e313dd1c005fab1fec51dad7ae282
Jean-Charles Gastaud <unice@jc.cx>
2013-01-07 09:33:00 +0100

    Finish sign in/out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+    sign_in @user
     flash[:success] = "Welcome to the Bib App!"
-      redirect_to @user
+    redirect_to @user
     else
       render 'new'
     end
