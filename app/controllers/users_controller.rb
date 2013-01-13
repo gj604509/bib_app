490623c7930d5e0e32e07cf68322020c53d235f5
Jean-Charles Gastaud <unice@jc.cx>
2013-01-13 17:16:16 +0100

    Finish users edit update index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+	before_filter :signed_in_user, only: [:index, :edit, :update]
+	before_filter :correct_user,   only: [:edit, :update]	
+	
+	def index
+		@users = User.paginate(page: params[:page])
+	end
+	
   def show
     @user = User.find(params[:id])
   end
@@ -17,4 +24,32 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    #@user = User.find(params[:id])
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
